975e00be1081c687842074873422173d469e47f8
Mayra Oliva <utm20040050@utma.edu.mx>
2022-06-24 13:02:11 -0500

    Se modifico ruteo de api

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,17 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { query } from '@angular/animations'
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
+  url = environment.urlNewsApi;
   constructor(private http: HttpClient) {
 
   }
 
 getNews(){
-return this.http.get("https://newsapi.org/v2/everything?q=tesla&from=2022-05-23&sortBy=publishedAt&apiKey=5e1e7d58fef242b4b0354c949518b9f4");
+  const query = {
+    q: "tesla",
+   from: "2022",
+   sortby: "published",
+   apiKey: "5e1e7d58fef242b4b0354c949518b9f4"
+  }
+
+  //this.url + "v2/everything"
+  return this.http.get(`${this.url}v2/everything`,{params:query});
 }
  ngOnInit(): void {
  }
