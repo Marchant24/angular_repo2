10610f3ffb892da1535753495cc1d9d594665c18
Mayra Oliva <utm20040050@utma.edu.mx>
2022-06-28 10:07:32 -0500

    Se crearon nuevas carpetas marvel

---
+++ src/app/models/marvel.model.ts
@@ -0,0 +1,14 @@
+import * as internal from "stream"
+
+export class marvelModel {
+    name: string = ""
+    nameStartsWith: string = ""
+    modifiedSince: string="" 
+    comis: string=""
+    series: string=""
+    events:string=""
+    stories: string=""
+    OrderBy: string=""
+    limit: string=""
+    
+}
